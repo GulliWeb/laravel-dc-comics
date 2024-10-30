38b301b0c35c27cb27751ee8ec131649f80f19a5
Andrea Gulli <andreagulli11@gmail.com>
2024-10-30 17:22:54 +0100

    improve comments & fixed syntax error on app.blade.php

--- resources/views/Supercars/layouts/app.blade.php
+++ resources/views/Supercars/layouts/app.blade.php
@@ -4,18 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yeld('title', 'Pagina di deafult')</title>
+    <title>@yield('title', 'Pagina di deafult')</title>
 </head>
 <body>
     <!-- Includo il codice che andrò a scrivere nei file partials -->
     <!-- Header -->
     @include('Supercars/layouts/partials.header')
 
+    <!-- Aggiungo il codice che verrà modificato in caso di più pagine tramite le section nel mio file pages -->
     <!-- Main -->
     <main>
         @yield('content')
     </main>
 
+    <!-- Includo il footer tramite il codice scritto nel file footer nei partials -->
     <!-- Footer -->
     @include('Supercars/Layouts/partials.footer')
 </body>
--- resources/views/Supercars/pages/home.blade.php
+++ resources/views/Supercars/pages/home.blade.php
@@ -1,7 +1,10 @@
+<!-- Estendo il Layout "base" del mio file layout app.blade.php
 @extends('Supercars/layouts.app')
 
-@section('title, home')
+<!-- Sostituisco, dove nel mio layout base (app.blade.php) c'è uno yield ('title') con questo codice -->
+@section('title', 'Supercars Showcase')
 
+<!-- Dove nel mio layout base (app.blade.php) c'è uno yield 'content' aggiungo questo codice -->
 @section('content')
 <h2>Benvenuto nella home</h2>
 @endsection
